1b55be67a1fedec1f3ca77cbb01d955c1d17af49
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2017-10-26 19:37:39 -0200

    Remanejando scripts de instalação

--- raspi-config.sh
+++ raspi-config.sh
@@ -2,10 +2,12 @@
 
 REPO="https://github.com/raspi-config/raspi-config.git"
 
+rm -rf /tmp/raspi-config
+
 git clone --quiet $REPO /tmp/raspi-config > /dev/null
 
 cd /tmp/raspi-config
 
-source scripts/install.sh
+source ./scripts/install.sh
 
 install_raspi_config
